44410ad51d9b1efbe8ffee75e9383cdacaa9591c
Kevin Nguyen <tuo17215@temple.edu>
2025-09-18 11:59:37 -0400

    adjust textSize to position in array

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,31 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(val number : Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>()/* TODO Step 3a: Provide Adapter Parent */ {
+    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {
+    }
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NumberViewHolder {
+        return NumberViewHolder(TextView(parent.context))
+    }
 
-    // TODO Step 3b: Complete function definitions for adapter
+    override fun onBindViewHolder(
+        holder: NumberViewHolder,
+        position: Int
+    ) {
+        holder.textView.text =  number[position].toString()
+        holder.textView.textSize = number[position].toString().toFloat()
+    }
 
+    override fun getItemCount(): Int {
+        return number.size
+    }
 }
